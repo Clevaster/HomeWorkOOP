0827b9616d6f120caa5025762869e78be6f2513b
Yana Vayzer <cc2332005@gmail.com>
2015-05-24 12:37:31 +0300

    Homework

--- src/Lesson3/URLparse/URLParse.java
+++ src/Lesson3/URLparse/URLParse.java
@@ -7,11 +7,15 @@ import java.util.Map;
  * Created by Lena on 23.05.2015.
  */
 public class URLParse {
-
-
-
+/*
+    Написать метод разбора списка параметров в формате URL:
+    para1=value1&param2=value2&param3=value3. В случае ошибки в
+    формате бросать исключение.
+*/
 
     public static void main(String[] args) {
+
+        // не понятно как вызвать parseURL(String url)  метод!!!
 
         String url = "para1=value1&param2=value2&param3=value3";
         try {
