ef2645cb3c79c4f1ab00f7b4c3fa159a77747193
Yana Vayzer <cc2332005@gmail.com>
2015-05-31 15:57:10 +0300

    Homework 5

--- src/lesson5/helloTask2/Hello.java
+++ src/lesson5/helloTask2/Hello.java
@@ -13,30 +13,43 @@ import java.io.IOException;
 public class Hello {
     public static void main(String[] args) {
 
-        try (FileInputStream fis = new FileInputStream(Constants.FILE_PATH_5 + "/1234.txt")) {
+        //reading stream
 
-            //Can't write into same file. File is empty is same path/
-            try (FileOutputStream fos = new FileOutputStream(Constants.FILE_PATH_5 + "/temp.txt")) {
+        try (FileInputStream fisReader = new FileInputStream(Constants.FILE_PATH_5 + "/1234.txt")) {
 
-                int size = fis.available();
-                byte[] bytes = new byte[size];
-                fis.read(bytes);
+            //size of file
+            int size = fisReader.available();
 
-                String textHello = new String(bytes);
+            //array for bytes fron file
+            byte[] bytes = new byte[size];
 
-                System.out.println("textHello: " + textHello);
+            // reading bytes from file into array
+            fisReader.read(bytes);
 
-                String text1234 = textHello.replaceAll("[Hh]ello", "1234");
-                System.out.println("text1234: " + text1234);
+            //Bytes array toString
+            String textHello = new String(bytes);
+
+            System.out.println("textHello: " + textHello);
+
+            String text1234 = textHello.replaceAll("[Hh]ello", "1234");
+            System.out.println("-----------------------");
+            System.out.println("text1234: " + text1234);
+
+            fisReader.close();
+
+            try (FileOutputStream fos = new FileOutputStream(Constants.FILE_PATH_5 + "/1234.txt")) {
 
                 byte[] out = text1234.getBytes();
                 fos.write(out);
 
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
         }
+
 
 
     }
